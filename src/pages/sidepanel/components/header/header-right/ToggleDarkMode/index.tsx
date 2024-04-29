06e7391bf3ff3cb337f4d28aeb52df7b87b8d603
chiragksharma <chiragksharma15@gmail.com>
2024-04-29 11:52:26 +0530

    added sun and moon icons to the toggle button

--- src/pages/sidepanel/components/header/header-right/ToggleDarkMode/index.tsx
+++ src/pages/sidepanel/components/header/header-right/ToggleDarkMode/index.tsx
@@ -1,9 +1,16 @@
 import '@pages/sidepanel/components/header/header-right/ToggleDarkMode/index.scss';
+import sunIcon from "@assets/img/sun_icon_light_mode.svg";
+import MoonIcon from "@assets/img/Moon_icon_Dark_mode.svg";
 
 const ToggleButton = ({ isDarkMode, onToggle }) => {
     return (
-      <div id="toggle" onClick={onToggle} style={{ backgroundColor: isDarkMode ? '#4A5568' : '#1a202c' }}>
-        <div className={`toggle-inner ${isDarkMode ? 'toggle-active' : ''}`} />
+      <div id="toggle" onClick={onToggle} className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+        {/* <div className={`toggle-inner ${isDarkMode ? 'toggle-active' : ''}`} /> */}
+        <img 
+        src={isDarkMode ? chrome.runtime.getURL(MoonIcon) : chrome.runtime.getURL(sunIcon)}
+        alt={isDarkMode ? "Dark Mode" : "Light Mode"}
+        className={`toggle-icon ${isDarkMode ? "dark-mode" : ""}`}
+      />
       </div>
     );
   };
